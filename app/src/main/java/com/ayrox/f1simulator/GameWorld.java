ff59cb97ca915b556e4e3f118856238656759c8e
Ayrox99 <56259172+Ayrox99@users.noreply.github.com>
2025-10-13 22:29:42 +0200

    Add GameWorld to manage simulation state

---
+++ app/src/main/java/com/ayrox/f1simulator/GameWorld.java
@@ -0,0 +1,17 @@
+package com.ayrox.f1simulator;
+
+import java.util.List;
+
+public class GameWorld {
+
+    int currentYear;
+
+    List<Driver> allDrivers;
+    List<Team> allTeams;
+    List<TechnicalChief> allTechnicalChiefs;
+
+    // On ajoutera plus tard les pilotes à la retraite, les jeunes qui attendent, etc.
+    // List<Driver> retiredDrivers;
+    // List<Driver> youngDriversPool;
+
+}
