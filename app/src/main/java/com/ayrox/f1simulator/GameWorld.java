a79f6ea670984281163762bb91c9f1c7e50e812d
Ayrox99 <56259172+Ayrox99@users.noreply.github.com>
2025-10-15 23:59:27 +0200

    Populate GameWorld with initial grid data

--- app/src/main/java/com/ayrox/f1simulator/GameWorld.java
+++ app/src/main/java/com/ayrox/f1simulator/GameWorld.java
@@ -1,5 +1,6 @@
 package com.ayrox.f1simulator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameWorld {
@@ -10,6 +11,51 @@ public class GameWorld {
     List<Team> allTeams;
     List<TechnicalChief> allTechnicalChiefs;
 
+    // NOUVEAU : Le constructeur de la classe
+    public GameWorld() {
+        // On déplace tout le code de création ici
+        this.currentYear = 2025;
+
+        // On initialise nos listes pour éviter les erreurs
+        this.allTeams = new ArrayList<>();
+        this.allDrivers = new ArrayList<>();
+        this.allTechnicalChiefs = new ArrayList<>();
+
+        // Création de l'écurie Mercedes
+        Team mercedes = new Team();
+        mercedes.name = "Mercedes-AMG Petronas";
+        mercedes.base = "Brackley, UK";
+        mercedes.budget = 500000000.0;
+        mercedes.drivers = new ArrayList<>();
+
+        // Création du pilote Hamilton
+        Driver hamilton = new Driver();
+        hamilton.firstName = "Lewis";
+        hamilton.lastName = "Hamilton";
+        hamilton.age = 40;
+        hamilton.pace = 95;
+
+        // Création de l'écurie Ferrari
+        Team ferrari = new Team();
+        ferrari.name = "Ferrari";
+        ferrari.base = "Maranello";
+        ferrari.budget = 500000000.0;
+        ferrari.drivers = new ArrayList<>();
+
+        // Création du pilote Leclerc
+        Driver leclerc = new Driver();
+        leclerc.firstName = "Charles";
+        leclerc.lastName = "Leclerc";
+        leclerc.age = 26;
+        leclerc.pace = 95;
+
+        // On lie le pilote à l'écurie et on ajoute l'écurie au monde
+        ferrari.drivers.add(leclerc);
+        mercedes.drivers.add(hamilton);
+        this.allTeams.add(ferrari);
+        this.allTeams.add(mercedes);
+
+    }
     // On ajoutera plus tard les pilotes à la retraite, les jeunes qui attendent, etc.
     // List<Driver> retiredDrivers;
     // List<Driver> youngDriversPool;
--- app/src/main/java/com/ayrox/f1simulator/MainActivity.java
+++ app/src/main/java/com/ayrox/f1simulator/MainActivity.java
@@ -1,12 +1,15 @@
 package com.ayrox.f1simulator;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // On crée notre monde, le constructeur fait tout le travail !
+        GameWorld world = new GameWorld();
+
+        // On utilise Log.d pour afficher le résultat (plus propre)
+        Log.d("MainActivity", "Monde créé pour l'année : " + world.currentYear);
+        Log.d("MainActivity", "Nombre d'écuries : " + world.allTeams.size());
+        for (int i = 0; i < world.allTeams.size(); i++) {
+            Log.d("MainActivity", "Ecurie " + i + " : " + world.allTeams.get(i).name);
+        }
     }
+
+
 }
